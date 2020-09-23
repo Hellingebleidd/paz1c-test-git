3e487ad2f39b0f41ac834d68860f5f85c6bf80d6
zuzanka <hellingebleidd@gmail.com>
2020-09-23 11:31:42 +0200

    dalsia zmena

--- src/prvaJava.java
+++ src/prvaJava.java
@@ -3,5 +3,7 @@ public class prvaJava {
 	public static void main(String[] args) {
 		//pridavam komentar len aby bola zmena
 		System.out.println("Zuzanka");
+		//robim dalsiu zmenu
+		System.out.println("dalsia zmena");
 	}
 }
