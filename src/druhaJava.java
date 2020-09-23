2d89e91cfffe6cd039a91a4616d940135f025e04
student <student@158.197.35.234>
2020-09-23 11:43:22 +0200

    Imposta

---
+++ src/druhaJava.java
@@ -0,0 +1,9 @@
+
+public class druhaJava {
+	public static void main(String[] args) {
+		//pridavam komentar len aby bola zmena
+		System.out.println("Yuyanka");
+		//robim dalsiu zmenu
+		System.out.println("Imposter");
+	}
+}
